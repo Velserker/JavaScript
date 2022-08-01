cdbdb2964985673deaee9a86cc7c0d14f082e48b
Pedro De Leon <velserker@gmail.com>
2022-08-01 16:07:28 -0500

    segunda entrega

--- js/app.js
+++ js/app.js
@@ -2,6 +2,22 @@
 let usuario
 let contraseña
 
+class ropa{
+    constructor(nombre, marca, precio){
+        this.nombre = nombre
+        this.marca = marca
+        this.precio = precio
+    }
+}
+let ropa1 = new ropa("Sueter", "Nike", 15)
+let ropa2 = new ropa("Pantalon", "Adidas", 20)
+let ropa3 = new ropa("Camisa", "Falabella", 25)
+let ropa4 = new ropa("Zapatos", "Reebok", 35)
+let ropa5 = new ropa("Medias", "Adidas", 8)
+let ropa6 = new ropa("Cinturon", "Rockefeller", 5)
+
+let ropas = [ropa1, ropa2, ropa3, ropa4, ropa5, ropa6]
+
 let opcion = Number(prompt("elige una opcion de la lista"));
 
 while(opcion > 0 && opcion <=3 ){
@@ -12,7 +28,10 @@ while(opcion > 0 && opcion <=3 ){
             contraseña = prompt("ingrese contraseña")
             break;
         case 2:
-            alert("Sueter 10$ - Pantalon 13$ - Camisa 15$ - Zapatos 20& - Medias 2$ - Cinturon 5&")
+            
+            for(let i of ropas){
+                console.log(i.nombre, i.marca, i.precio)
+            }
             break;
         case 3:
             let producto, contador=0;
@@ -23,22 +42,22 @@ while(opcion > 0 && opcion <=3 ){
             for(let i = 1; i <= nP ; i++){
                 producto = prompt("escribe el nombre de tu producto numero "+ i).toLowerCase();
                 if (producto == "sueter"){
-                    producto = 10;
+                    producto = ropa1.precio
                     contador = suma();
                 }else if (producto == "pantalon"){
-                    producto = 13;
+                    producto = ropa2.precio
                     contador = suma();
                 }else if (producto == "camisa"){
-                    producto = 15;
+                    producto = ropa3.precio
                     contador = suma();
                 }else if (producto == "zapatos"){
-                    producto = 20;
+                    producto = ropa4.precio
                     contador = suma();
                 }else if (producto == "medias"){
-                    producto = 2;
+                    producto = ropa5.precio
                     contador = suma();
                 }else if (producto == "cinturon"){
-                    producto = 5;
+                    producto = ropa6.precio
                     contador = suma();
                 }else{
                     alert("Escribe el nombre de un producto de la lista");
