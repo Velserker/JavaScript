56426691d61eed23e02a1f63aef2bdb55d339cac
Pedro De Leon <velserker@gmail.com>
2022-08-22 23:38:03 -0500

    segunda entrega

--- js/app.js
+++ js/app.js
@@ -2,21 +2,39 @@
 let usuario
 let contraseña
 
-class ropa{
-    constructor(nombre, marca, precio){
+class burguer{
+    constructor(nombre, imgSrc, ingredientes, precio){
         this.nombre = nombre
-        this.marca = marca
+        this.imgSrc = imgSrc
+        this.ingredientes = ingredientes
         this.precio = precio
     }
 }
-let ropa1 = new ropa("Sueter", "Nike", 15)
-let ropa2 = new ropa("Pantalon", "Adidas", 20)
-let ropa3 = new ropa("Camisa", "Falabella", 25)
-let ropa4 = new ropa("Zapatos", "Reebok", 35)
-let ropa5 = new ropa("Medias", "Adidas", 8)
-let ropa6 = new ropa("Cinturon", "Rockefeller", 5)
+const burguer1 = new burguer("Clasica",'https://www.atriumpizzayburger.com/sites/default/files/images/productos/tradicional.jpg', "Pan brioche, 150g de carne Certified Angus Beef ®, Queso mozarella, Lechuga, Tomate", 17)
+const burguer2 = new burguer("Gaucha", "https://www.atriumpizzayburger.com/sites/default/files/images/productos/gaucha.jpg","Pan brioche, 150g de carne Certified Angus Beef ®, Queso mozarella, Chorizo argentino, Chimichurri, Tomate, Cebolla grillé, Salsa de la casa", 21)
+const burguer3 = new burguer("Americana", "https://www.atriumpizzayburger.com/sites/default/files/images/productos/_mg_1550_compress.jpg", "Pan brioche, 150g de carne Certified Angus Beef ®, Queso cheddar, Tocineta, Pepinillos, Lechuga, Tomate, Cebolla grillé, Salsa de la casa", 21)
+const burguer4 = new burguer("Doble toque", "https://www.atriumpizzayburger.com/sites/default/files/images/productos/doble_troque.jpg", "Pan brioche, 300g de carne Certified Angus Beef ®, Doble queso americano, Doble tocineta, Cebolla crunch, Lechuga crespa, Tomate fresco, Salsa de la casa", 30)
+const burguer5 = new burguer("Suprema", "https://www.atriumpizzayburger.com/sites/default/files/images/productos/la_fantastica.jpg", "Pan brioche, 150g de carne Certified Angus Beef ®, Queso cheddar, Tocineta, Huevo, Lechuga, Tomate, Cebolla grillé, Salsa de la casa", 8)
 
-let ropas = [ropa1, ropa2, ropa3, ropa4, ropa5, ropa6]
+
+const burguers = [burguer1, burguer2, burguer3, burguer4, burguer5]
+
+const cardContainerQuery = document.querySelector('#cardContainer')
+
+burguers.forEach((bur) => {
+    const cardProducto = document.createElement('div')
+    cardProducto.innerHTML = `
+        <h3 class="cardTitle"> Hamburguesa ${bur.nombre} </h3>
+        <img src="${bur.imgSrc}" class="cardImg">
+        <p class="cardDesc"> ${bur.ingredientes}</p>
+        <span class="cardPrice"> $${bur.precio} Cop </span>
+        <button class="buttonCTA"> Agregar al Carrito </button>
+    `
+    cardProducto.className = 'card'
+    cardContainerQuery.append(cardProducto)
+})
+
+
 
 let opcion = Number(prompt("elige una opcion de la lista"));
 
@@ -28,42 +46,18 @@ while(opcion > 0 && opcion <=3 ){
             contraseña = prompt("ingrese contraseña")
             break;
         case 2:
-            
-            for(let i of ropas){
-                console.log(i.nombre, i.marca, i.precio)
-            }
-            break;
-        case 3:
-            let producto, contador=0;
-            function suma(){
-                return contador + producto
-            }
-            let nP = prompt("cuantos productos desea comprar");
+            let producto
+            var precioTotal = 0
+            const nP = window.prompt("cuantos productos desea comprar");
             for(let i = 1; i <= nP ; i++){
                 producto = prompt("escribe el nombre de tu producto numero "+ i).toLowerCase();
-                if (producto == "sueter"){
-                    producto = ropa1.precio
-                    contador = suma();
-                }else if (producto == "pantalon"){
-                    producto = ropa2.precio
-                    contador = suma();
-                }else if (producto == "camisa"){
-                    producto = ropa3.precio
-                    contador = suma();
-                }else if (producto == "zapatos"){
-                    producto = ropa4.precio
-                    contador = suma();
-                }else if (producto == "medias"){
-                    producto = ropa5.precio
-                    contador = suma();
-                }else if (producto == "cinturon"){
-                    producto = ropa6.precio
-                    contador = suma();
-                }else{
-                    alert("Escribe el nombre de un producto de la lista");
-                }         
+                burguers.forEach((Hnombre) => {
+                    if (Hnombre.nombre.toLowerCase() === producto){
+                        precioTotal += Hnombre.precio 
+                    }
+                })       
             }
-            alert("El total de tu compra es "+ contador + "&")
+            alert("El total de tu compra es "+ precioTotal + " Cop");
             break;
         default:
             alert("seleccione una opcion de la lista")
